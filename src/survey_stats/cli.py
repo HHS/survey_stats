a8a3378c92932f6ebf5bbb46af0ab76a1c3279ab
Ajish George <ajish@rootedinsights.com>
2017-01-18 05:39:24 -0500

    integrate gunicorn, create command-line runner, breakout settings

--- src/survey_stats/cli.py
+++ src/survey_stats/cli.py
@@ -3,25 +3,59 @@ Module that contains the command line app.
 
 Why does this file exist, and why not put this in __main__?
 
-  You might be tempted to import things from __main__ later, but that will cause
-  problems: the code will get executed twice:
+You might be tempted to import things from __main__ later, but that will cause
+problems: the code will get executed twice:
 
-  - When you run `python -msurvey_stats` python will execute
-    ``__main__.py`` as a script. That means there won't be any
-    ``survey_stats.__main__`` in ``sys.modules``.
-  - When you import __main__ it will get executed again (as a module) because
-    there's no ``survey_stats.__main__`` in ``sys.modules``.
+- When you run `python -msurvey_stats` python will execute
+``__main__.py`` as a script. That means there won't be any
+``survey_stats.__main__`` in ``sys.modules``.
+- When you import __main__ it will get executed again (as a module) because
+there's no ``survey_stats.__main__`` in ``sys.modules``.
 
-  Also see (1) from http://click.pocoo.org/5/setuptools/#setuptools-integration
+Also see (1) from http://click.pocoo.org/5/setuptools/#setuptools-integration
 """
+
+import os
 import argparse
+from survey_stats.server import APIServer, number_of_workers
+from survey_stats.api import app as api
 
+parser = argparse.ArgumentParser(description='Unified Survey Stats Repository')
+subparsers = parser.add_subparsers()
 
-parser = argparse.ArgumentParser(description='Command description.')
-parser.add_argument('names', metavar='NAME', nargs=argparse.ZERO_OR_MORE,
-                    help="A name of something.")
+parser_serve = subparsers.add_parser('serve')
+parser_serve.add_argument('--host',
+                          default = os.environ.get('HOST', '0.0.0.0'),
+                          help = 'interface to bind API service, default: 0.0.0.0')
+parser_serve.add_argument('--port', type=int,
+                          default = os.environ.get('PORT', 7777),
+                          help = 'port for API service, default: 7777')
+parser_serve.add_argument('--workers', type=int,
+                          default = number_of_workers(),
+                          help = 'number of worker processes, default: num_cores/2')
+parser_serve.add_argument('--max-requests', type=int,
+                          default = 1,
+                          help = 'requests to serve per worker before respawn, default: 1')
+parser_serve.add_argument('--max-requests-jitter', type=int,
+                          default = 3,
+                          help = 'max jitter to add to max requests, default: 3')
 
+def default_action(args):
+    parser.print_help()
+
+def serve_api(args):
+    options = {
+        'bind': '%s:%s' % (args.host, str(args.port)),
+        'workers': args.workers,
+        'max_requests': args.max_requests,
+        'max_requests_jitter': args.max_requests_jitter,
+        'when_ready': boot_when_ready
+    }
+    APIServer(api, options).run()
+
+parser_serve.set_defaults(func=serve_api)
+parser.set_defaults(func=default_action)
 
 def main(args=None):
     args = parser.parse_args(args=args)
-    print(args.names)
+    args.func(args)
---
+++ src/survey_stats/settings.py
@@ -0,0 +1,2 @@
+BACKTRACE_URL='https://rootedinsights.sp.backtrace.io:6098'
+BACKTRACE_TKN='768367df71e2be15321e851494b6dcc7152bf8f48fe5cc85a2deac80b94a31e9'
