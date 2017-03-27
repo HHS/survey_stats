058af128055488cc395f4eb205519754cfd7e6a4
Ajish George <ajish@rootedinsights.com>
2017-03-27 09:59:19 -0400

    brfs precomputed live

--- src/survey_stats/state.py
+++ src/survey_stats/state.py
@@ -7,10 +7,12 @@ meta = {}
 dset = {}
 
 #load survey datasets
+dset['brfs'] = YRBSSDataset.load_dataset('data/brfs.yaml')
 dset['prams'] = YRBSSDataset.load_dataset('data/prams.yaml')
 dset['yrbss'] = YRBSSDataset.load_dataset('data/yrbss.yaml')
 
 #fetch the state.metadata from Socrata
+dset['brfs'] = YRBSSDataset.load_dataset('data/brfs.yaml')
 meta['prams'] = SurveyMetadata.load_metadata('data/prams.yaml')
 meta['yrbss'] = SurveyMetadata.load_metadata('data/yrbss.yaml')
 
