2c9f030400c65c0766559ddf4eb3d1e37ac99474
Morgan Thompson <anniemorgan@users.noreply.github.com>
2017-06-26 17:13:46 -0700

    Update helpr.py

--- src/survey_stats/helpr.py
+++ src/survey_stats/helpr.py
@@ -7,7 +7,7 @@ function(formula, design, method='xlogit', level = 0.95, df=degf(design), ...) {
 
 svybyci_yrbs = robjects.r('''
 function( formula, by, des, fn, ...) {
-    svyby(formula, by, des, fn, keep_var=TRUE, method='xlogit',
+    svyby(formula, by, des, fn, keep.var=TRUE, method='xlogit',
           vartype=c('se','ci'), na.rm.by=TRUE, na.rm.all=TRUE, multicore=TRUE)
 }''')
 
