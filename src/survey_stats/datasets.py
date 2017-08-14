a34ecb92111b3464e09a2f7eb0d1de4b4ef6d742
Ajish George <ajish@rootedinsights.com>
2017-08-13 20:27:27 -0400

    update imports, remove unnecessary methods

--- src/survey_stats/datasets.py
+++ src/survey_stats/datasets.py
@@ -1,75 +1,53 @@
-import os
-import yaml
-import ujson as json
-import rpy2
-from rpy2 import robjects
-from rpy2.robjects.packages import importr
+import blaze as bz
+import feather
 from collections import namedtuple
 
-from survey_stats.log import logger
+from survey_stats import log
 from survey_stats.survey import AnnotatedSurvey
-from survey_stats.feathers import has_feather, load_feather
+from survey_stats.serdes import has_feather, load_feather
 
-rfther = importr('feather', on_conflict='warn')
-rutils = importr('utils')
-rbase = importr('base')
+STATS_COLUMNS = ['count','mean','ci_u','ci_l','se']
 
-cache_dir = os.path.join(os.getcwd(), 'cache')
+logger = log.getLogger()
+
+def resolve_db_url(url):
+    return (feather.read_dataframe(url) if
+            url.endswith('.feather') else
+            bz.data(url))
 
 
-class SurveyDataset(namedtuple('Dataset', ['config', 'surveys'])):
-    __slots__ = ()
+
+class SurveyDataset(namedtuple('SurveyDataset',
+                               ['svy_db','meta_db', 'cfg'])):
 
     @classmethod
-    def load_dataset(cls, yml_f):
-        config = None
-        with open(yml_f, 'r') as fh:
-            config = yaml.load(fh)['surveys']
-        if not config:
-            return cls(config=None, surveys=None)
-        svys = {}
-        for k, v in config.items():
-            svys[k] = cls.fetch_or_load_dataset(k, v['spss'], v['data'])
-
-        return cls(config=config, surveys=svys)
-
-    def fetch_or_load_dataset(id, spss_f, data_f):
-        f = os.path.join(cache_dir, '%s.feather' % id)
-        ret = None
-        rdf = None
-        try:
-            rdf = rfther.read_feather(f)
-            ret = AnnotatedSurvey.from_rdf(spss_f, rdf)
-            logger.info('loaded survey data from feather cache: %s' % f)
-        except:
-            logger.info('could not find feather cache, loading raw data')
-            ret = AnnotatedSurvey.load_cdc_survey(spss_f, data_f)
-            logger.info('saving data to feather cache: %s' % f)
-            rfther.write_feather(ret.rdf, f)
-
-        rbase.gc()
-        return ret
+    def from_db_urls(cls, svy_url, meta_url, cfg):
+        return cls(svy_db=resolve_db_url(svy_url),
+                   meta_db=resolve_db_url(meta_url), cfg=cfg)
 
 
-class YRBSSDataset(SurveyDataset):
-    __slots__ = ()
-
-    def fetch_survey(self, combined=True, national=True, year=None):
-        pred = (lambda v: v['is_combined'] == combined and
-                (v['year'] == year if year else True))
-        if not self.config:
-            return None
-        return next(( self.surveys[k] for k, v in self.config.items() if pred(v)), None)
-
-    def fetch_config(self, national=True, year=None):
-        combined = not year # if year is present, get ind year, else combined
-        pred = (lambda v: v['is_combined'] == combined and
-                (v['year'] == year if year else True))
-        if not self.config:
-            return None
-        return next(((k, v) for k, v in self.config.items() if pred(v)), None)
+    def fetch_socrata(self, qn, vars, filt = {}):
+        sel = df.qid == qn
+        if 'sitecode' in filt.keys():
+            sel = sel & df.sitecode.isin(filt['sitecode'])
+        if 'year' in filt.keys():
+            sel = sel & df.year.isin(map( int, filt['year']))
+        for v in self.facets.keys():
+            if v in filt.keys():
+                sel = sel & df[v].isin(filt[v])
+            if not v in vars:
+                sel = sel & (df[v] == 'Total')
+        cols = ['qid','response','sitecode','year'] + \
+                self.facets + STATS_COLUMNS
+        return df.to_dict(orient='records')
 
 
-    @property
-    def survey_years(self):
-        return set(sorted([int(v['year']) for v in self.config.values()]))
+    def facets(self):
+        return self.cfg['facets']
+
+
+    def facet_levels(self):
+        return {k: list(self.meta_db[k]
+                        .cat.categories) for k in self.facets}
+
+
