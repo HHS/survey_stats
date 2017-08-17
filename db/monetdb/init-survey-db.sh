4e78ebd7ced6fe68f6502774325ceeb0764eee14
Ajish George <ajish@rootedinsights.com>
2017-08-17 10:11:34 -0400

    added latest monetdb dockerfile

--- db/monetdb/init-survey-db.sh
+++ db/monetdb/init-survey-db.sh
@@ -2,6 +2,8 @@
 
 # TODO: fetch static db files from network into survey dir
 
+monetdbd start /var/monetdb5/dbfarm
+
 sleep 5
 if [ ! -d "/var/monetdb5/dbfarm/survey" ]; then
     monetdb create survey && \
@@ -10,6 +12,22 @@ if [ ! -d "/var/monetdb5/dbfarm/survey" ]; then
     monetdb release survey
 else
     echo "Existing database found in '/var/monetdb5/dbfarm/survey'"
+fi
+
+for i in {30..0}; do
+    echo 'Testing MonetDB connection ' $i
+    mclient -d db -s 'SELECT 1' &> /dev/null
+    if [ $? -ne 0 ] ; then
+      echo 'Waiting for MonetDB to start...'
+      sleep 1
+    else
+        echo 'MonetDB is running'
+        break
+    fi
+done
+if [ $i -eq 0 ]; then
+    echo >&2 'MonetDB startup failed'
+    exit 1
 fi
 
 # TODO: monetdb set readonly=yes survey
