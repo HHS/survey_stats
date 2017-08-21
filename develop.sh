4c2000ad981c3359d61f9fc1104e2673aaa53eaa
Ajish George <ajish@rootedinsights.com>
2017-08-21 00:20:41 -0400

    test hooks

--- develop.sh
+++ develop.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+CURDIR=`pwd`
+
 # setup miniconda3
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh \
     -O miniconda.sh
@@ -16,10 +18,10 @@ conda info -a
 conda config --add channels intel
 
 # create conda env with intel python 3.6 and gnu r 3.4.1
-conda create -q -n ssdev-intelpy36gnur34 -c intel/label/test python=3 r-base=3.4.1 pandas scikit-learn cython r-feather libiconv r-survival r-dbi
+conda create -q -p venv -c intel/label/test python=3 r-base=3.4.1 pandas scikit-learn cython r-feather libiconv r-survival r-dbi
 
 # activate the env and install package and dev requirements with pip
-source activate ssdev-intelpy36gnur34
+source activate $CURDIR/venv
 pip install -r requirements-dev.txt
 pip install -e .
 
--- tests/test_etl.py
+++ tests/test_etl.py
@@ -1,4 +1,3 @@
 import pandas as pd
 
-def test_subset_survey():
-    pass
+
