b26635547b2676075a4968af81ad161c1391642a
Ajish George <ajish@rootedinsights.com>
2017-08-26 23:01:24 -0400

    fix init script and relative dirs

--- db/monetdb/init-survey-db.sh
+++ db/monetdb/init-survey-db.sh
@@ -2,21 +2,21 @@
 
 # TODO: fetch static db files from network into survey dir
 
-monetdbd start /var/monetdb5/dbfarm
+monetdbd start /data
 
 sleep 5
-if [ ! -d "/var/monetdb5/dbfarm/survey" ]; then
+if [ ! -d "/data/survey" ]; then
     monetdb create survey && \
     monetdb set embedr=true survey && \
     monetdb set embedpy=true survey && \
     monetdb release survey
 else
-    echo "Existing database found in '/var/monetdb5/dbfarm/survey'"
+    echo "Existing database found in '/data/survey'"
 fi
 
 for i in {30..0}; do
     echo 'Testing MonetDB connection ' $i
-    mclient -d db -s 'SELECT 1' &> /dev/null
+    mclient -d survey -s 'SELECT 1' &> /dev/null
     if [ $? -ne 0 ] ; then
       echo 'Waiting for MonetDB to start...'
       sleep 1
