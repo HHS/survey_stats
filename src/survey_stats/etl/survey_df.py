2752ac631eb540ed8c02c5be91a6c3db3e8776c3
Ajish George <ajish@rootedinsights.com>
2017-08-26 22:50:31 -0400

    break out category conversion logic to find breaking decision branches

--- src/survey_stats/etl/survey_df.py
+++ src/survey_stats/etl/survey_df.py
@@ -1,20 +1,14 @@
-import sys
 import us
 import pandas as pd
 import numpy as np
-from cytoolz.itertoolz import mapcat
+from cytoolz.itertoolz import unique
 from cytoolz.functoolz import thread_last
 from cytoolz.curried import map, filter, curry
-import traceback as tb
-import dask
-from dask import distributed as dd
-from dask import delayed
-from dask import bag
-from dask.distributed import Client
+# import sys
+# import traceback as tb
 
 
 from survey_stats import log
-from survey_stats import pdutil
 
 
 logger = log.getLogger(__name__)
@@ -35,40 +29,61 @@ SITECODE_TRANSLATORS = {
 
 SVYDESIGN_COLS = ['sitecode', 'strata', 'psu', 'weight']
 
-def force_convert_categorical(s, lbls):
-    c = (pd.to_numeric(s.fillna(-1), downcast='integer')
-         .replace(to_replace=lbls[s.name])
-         .astype('category'))
+
+def convert_cat_codes(s, fmt):
+    unq_lvls = list(unique(
+        [fmt[k] for k in sorted(fmt.keys())]
+    ))
+    c = (pd.to_numeric(s, downcast='integer')
+         .astype('category')
+         .pipe(lambda xf: xf.cat.rename_categories(
+             [fmt[k] for k in sorted(xf.unique().dropna())]))
+         .cat.set_categories(unq_lvls))
     return c
 
 
-def eager_convert_categorical(s, lbls):
-    if not s.name in lbls.keys():
-        return s
+def convert_cat_force(s, fmt):
+    unq_lvls = list(unique(
+        [fmt[k] for k in sorted(fmt.keys())]
+    ))
+    c = (pd.to_numeric(s, downcast='integer')
+         .replace(to_replace=fmt)
+         .astype('category')
+         .cat.set_categories(unq_lvls))
+    return c
+
+
+def eager_convert(s, fmt, lgr=logger):
+    c = s
     try:
-        c = (pd.to_numeric(s, downcast='integer')
-             .astype('category')
-             .pipe(lambda xf: xf.cat.rename_categories(
-                        [lbls[s.name][k] for k in
-                         sorted(xf.unique().dropna())]))
-             .cat.set_categories(
-                [lbls[s.name][k] for k in sorted(lbls[s.name].keys())])
-             .astype('category'))
-        return c
-    except KeyError as e:
-        exc_type, exc_value, exc_traceback = sys.exc_info()
-        xcep = tb.format_exception(exc_type,exc_value, exc_traceback)
-        logger.warning('KeyError casting to cat, check var labels! Passing...',
-                       err=e, col=s.name, v=s.value_counts().to_dict())
-        return s
+        c = convert_cat_codes(s, fmt)
+        lgr.info('converted series with fmt', v=s.name, fmt=fmt,
+                 levels=s.value_counts().to_dict())
     except ValueError as e:
-        exc_type, exc_value, exc_traceback = sys.exc_info()
-        logger.warning('ValueError converting to category! Forcing...',
-                       err=e, v=s.value_counts().to_dict())
-        return force_convert_categorical(s, lbls)
+        lgr.warning('ValueError converting to category! Forcing...',
+                    err=e, col=s.name, fmt=fmt, levels=s.value_counts().to_dict())
+        c = convert_cat_force(s, fmt)
+    except KeyError as e:
+        lgr.warning('KeyError casting to cat, check var labels! Passing...',
+                    err=e, col=s.name, fmt=fmt, levels=s.value_counts().to_dict())
+    return c
 
 
-def filter_columns(df, facets, qids):
+def eager_convert_categorical(s, lbls, fmts, lgr=logger):
+    fmt = None
+    if s.name in lbls.keys():
+        fmt = lbls[s.name]
+    elif s.name in fmts:
+        fmt = fmts[s.name]
+        lgr.info('found missing format to patch', v=s.name, fmt=fmt,
+                 levels=s.value_counts().to_dict())
+    if not fmt:
+        lgr.info('could not find fmt for var, skipping', v=s.name)
+        return s
+    return eager_convert(s, lbls[s.name], lgr)
+
+
+def filter_columns(df, facets, qids, lgr=logger):
     # should drop columns w/ facet names
     # unless the mapped value is the same
     drop_cols = set(facets.values()).difference(facets.keys())
@@ -80,9 +95,9 @@ def filter_columns(df, facets, qids):
     cols = sorted(fcols.intersection(df.columns).difference(drop_cols))
 
     ndf = df[cols]
-    logger.info('filtered df columns using facets, qids',
-                old_shape=df.shape, new_shape=ndf.shape,
-                missing=fcols.difference(cols), dropped=drop_cols)
+    lgr.info('filtered df columns using facets, qids',
+             old_shape=df.shape, new_shape=ndf.shape,
+             missing=fcols.difference(cols), dropped=drop_cols)
     return ndf
 
 
@@ -93,32 +108,34 @@ def find_na_synonyms(na_syns, df):
         else x)
     return df
 
+
 def undash(col):
     return 'x' + col if col[0] == '_' else col
 
-def munge_df(df, r, lbls, facets, qids, na_syns):
-    year=r['year']
-    logger.bind(year=year)
-    logger.info('filtering, applying varlabels, munging')
-    #lbls = {k:v for k,v in lbls.items()} ## if k in delayed(df.columns)}
+
+def munge_df(df, r, lbls, facets, qids, na_syns, fmts, lgr=logger):
+    year = r['year']
+    lgr.bind(year=year)
+    lgr.info('filtering, applying varlabels, munging')
+    # lbls = {k:v for k,v in lbls.items()} ## if k in delayed(df.columns)}
     # get mapping into table for each facet
-    facets = {r[k]:k for k in facets}
+    facets = {r[k]: k for k in facets}
     ndf = (df.pipe(lambda xdf: filter_columns(xdf, facets, qids))
            .reset_index(drop=True)
-           .apply(lambda x: eager_convert_categorical(x, lbls))
+           .apply(lambda x: eager_convert_categorical(x, lbls, fmts, lgr))
            .rename(index=str, columns=facets)
            .pipe(curry(find_na_synonyms)(na_syns))
            .pipe(lambda xf: xf.rename(index=str, columns={x: undash(x) for x in xf.columns}))
            .reset_index(drop=True)
-           .assign(year = int(year) if type(year) == int else df[year].astype(int),
-                   sitecode = df[r['sitecode']].apply(
+           .assign(year=int(year) if type(year) == int else df[year].astype(int),
+                   sitecode=df[r['sitecode']].apply(
                        SITECODE_TRANSLATORS['fips']).astype('category'),
-                   weight = df[r['weight']].astype(float),
-                   strata = df[r['strata']].astype(int),
-                   psu = df[r['psu']].astype(int))
+                   weight=df[r['weight']].astype(float),
+                   strata=df[r['strata']].astype(int),
+                   psu=df[r['psu']].astype(int))
            .reset_index(drop=True))
-    logger.info('completed SAS df munging')
-    logger.unbind('year')
+    lgr.info('completed SAS df munging')
+    lgr.unbind('year')
     return ndf
 
 
