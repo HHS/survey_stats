7e4b11139349db865430ea13585548d03086cd56
Ajish George <ajish@rootedinsights.com>
2017-08-26 22:59:28 -0400

    add stubs for new replace_labels and patch_format sections for survey processing

--- src/survey_stats/etl/load.py
+++ src/survey_stats/etl/load.py
@@ -3,17 +3,17 @@ import re
 import os
 
 import pandas as pd
-import pymonetdb
-import pymysql.cursors
 import sqlalchemy as sa
-import subprocess as sp
-import feather
-import pyarrow as pa
-
-from timeit import default_timer as timer
+# import pyarrow as pa
+import fastparquet as fpq
 import dask
-from dask import delayed
-from dask.distributed import Client
+import dask.delayed as delayed
+import dask.multiprocessing
+import dask.cache
+# from dask.distributed import LocalCluster, Client
+from multiprocessing.pool import ThreadPool
+from timeit import default_timer as timer
+
 from survey_stats import log
 from survey_stats.etl.sas import process_sas_survey
 from survey_stats.etl.socrata import fetch_socrata_stats
@@ -37,16 +37,19 @@ def load_socrata_data(yaml_f, client=None):
                                         apply_fn=params['apply_fn'],
                                         c_filter=params['c_filter'],
                                         unstack=params['unstack'],
-                                        fold_stats=params['fold_stats']) for url in params['soda_api']]
+                                        fold_stats=params['fold_stats']
+                                        ) for url in params['soda_api']]
     dfs = delayed(pd.concat)(dfs, ignore_index=True)
-    logger.info('converting df to csv table', name=ksoda)
-    serdes.save_csv(ksoda, dfs, index=False)
-    logger.info('converting df to pyarrow table', name=ksoda)
-    tbl = pa.Table.from_pandas(dfs)
+    dfs = dfs.compute()
+    # logger.info('converting df to csv table', name=ksoda)
+    # serdes.save_csv(ksoda, dfs, index=False)
+    # logger.info('converting df to pyarrow table', name=ksoda)
+    # tbl = pa.Table.from_pandas(dfs)
     logger.info('saving survey data to parquet', name=ksoda)
-    pa.write_table(tbl, 'cache/'+ksoda+'.parquet')
+    fpq.write('cache/'+ksoda+'.parquet', data=dfs, write_index=False,
+              has_nulls=True)
     logger.info('saving socrata data')
-    res = dfs.to_feather('cache/'+ksoda+'.feather')
+    dfs.to_feather('cache/'+ksoda+'.feather')
     return dfs
 
 
@@ -56,25 +59,35 @@ def load_survey_data(yaml_f, client=None):
         cfg = yaml.load(fh)
     logger.bind(dataset=cfg['id'])
     ksvy = serdes.surveys_key4id(cfg['id'])
-    logger.info('loading survey dfs')
+    logger.info('loading survey dfs', sk=cfg['surveys'].keys())
     svydf = process_sas_survey(meta=cfg['surveys']['meta'],
                                prefix=cfg['surveys']['s3_url_prefix'],
                                qids=cfg['surveys']['qids'],
                                facets=cfg['facets'],
                                na_syns=cfg['surveys']['na_synonyms'],
-                               client=client)
+                               repl=cfg['surveys']['replace_labels'],
+                               client=client, lgr=logger)
     logger.info('loaded survey dfs', shape=svydf.shape)
-    logger.info('dumping to csv for bulk load')
-    csvf = serdes.save_csv(ksvy, svydf, index=False)
-
-    logger.info('converting df to pyarrow table', name=ksvy)
-    tbl = pa.Table.from_pandas(svydf)
-    logger.info('saving survey data to parquet', name=ksvy)
-    pa.write_table(tbl, 'cache/'+ksvy+'.parquet')
-    logger.info('finally, saving survey data to feather', name=ksvy)
-    logger.info('saved as parquet and csv', csv=csvf)
+    svydf = svydf.compute()
+    svydf = svydf.reset_index(drop=True)
+    logger.info('dumping to csv for bulk load',
+                df=(svydf.select_dtypes(include=['object', 'category'])
+                         .apply(lambda xf: xf.value_counts().to_dict())
+                         .to_json(orient='index')))
+    # csvf = serdes.save_csv(ksvy, svydf, index=False)
+    csvt = timer()
+    # logger.info('dumped to csv for bulk load', elapsed=csvt-st)
+    # logger.info('converting df to pyarrow table', name=ksvy)
+    # tbl = pa.Table.from_pandas(svydf)
+    # logger.info('saving survey data to parquet', name=ksvy)
+    # pa.write_table(tbl, 'cache/'+ksvy+'.parquet')
+    fpq.write(filename='cache/'+ksvy+'_2.parquet', data=svydf,
+              has_nulls=True)
+    pqt = timer()
+    logger.info('finally, saving survey data to feather', name=ksvy, elapsed=pqt-csvt)
     logger.info('saving survey data feather.write_dataframe', name=ksvy)
     svydf.to_feather('cache/'+ksvy+'.feather')
+    logger.info('saved survey data feather.write_dataframe', name=ksvy, elapsed=timer()-pqt)
     logger.unbind('dataset')
     return svydf
 
@@ -93,11 +106,11 @@ def load_csv_mariadb_columnstore(df, tblname, engine):
 
 def load_csv_monetdb(df, tblname, engine):
     copy_tmpl = "COPY {nrows} OFFSET 2 RECORDS INTO {tbl} from '{csvf}'" + \
-                " USING DELIMITERS ','"
+                " USING DELIMITERS ', '"
     logger.info('creating schema for column store', name=tblname)
     start = timer()
     q = pd.io.sql.get_schema(df[:0], tblname, con=engine)
-    q = q.replace('\n',' ').replace('\t',' ')
+    q = q.replace('\n', ' ').replace('\t', ' ')
     logger.info('dumping to csv for bulk load', q=q)
     csvf = serdes.save_csv(tblname, df, index=False)
     csvf = os.path.abspath(csvf)
@@ -143,22 +156,25 @@ def setup_tables(yaml_f, dburl):
 
 
 if __name__ == '__main__':
-    from distributed import Client, LocalCluster
-    import dask.multiprocessing
-    from multiprocessing.pool import ThreadPool
     default_sql_conn = 'mysql+pymysql://mcsuser:mcsuser@localhost:4306/survey'
     default_sql_conn = 'monetdb://monetdb:monetdb@localhost/survey'
-    #lc = LocalCluster()
-    #client = Client(lc)
-    client=None
-    #dask.set_options(get=dask.threaded.get, pool=ThreadPool(16))
+    # lc = LocalCluster()
+    # client = Client(lc)
+    client = None
+    # cache = dask.cache.Cache(8e9)
+    # cache.register()
+    dask.set_options(get=dask.threaded.get, pool=ThreadPool())
+    # load_socrata_data('config/data/brfss.yaml', client)
     load_survey_data('config/data/brfss.yaml', client)
-    load_socrata_data('config/data/brfss.yaml',client)
+    load_socrata_data('config/data/brfss_pre2011.yaml', client)
+    load_survey_data('config/data/brfss_pre2011.yaml', client)
+    load_socrata_data('config/data/yrbss.yaml', client)
+    load_survey_data('config/data/yrbss.yaml', client)
+    '''
     setup_tables(
         'config/data/brfss.yaml',
         default_sql_conn
     )
-    '''
     setup_tables(
         'config/data/brfss_pre2011.yaml',
         'mysql+pymysql://mcsuser:mcsuser@localhost:4306/survey'
--- src/survey_stats/etl/sas.py
+++ src/survey_stats/etl/sas.py
@@ -1,25 +1,18 @@
-import os
-import os.path
 import io
 import re
-from collections import namedtuple
+import zipfile
 import pandas as pd
-import numpy as np
-from cytoolz.itertoolz import concat, concatv, mapcat
+from cytoolz.itertoolz import mapcat
 from cytoolz.curried import map, filter, curry
-from cytoolz.functoolz import pipe, thread_first, thread_last
-import dask
-from dask import distributed as dd
+from cytoolz.functoolz import pipe, thread_last
 from dask import delayed
-from dask import bag
-from dask.distributed import Client
-
 
 from survey_stats import log
 from survey_stats.etl import survey_df as sdf
 from survey_stats.etl import download as dl
 
 logger = log.getLogger(__name__)
+
 
 def parse_format_assignments(txt):
     format_lines = ''
@@ -33,11 +26,11 @@ def parse_format_assignments(txt):
         elif not append and l.startswith('format'):
             # begin collecting format lines
             append = True
-            format_lines += l.replace('format','',1) + ' '
+            format_lines += l.replace('format', '', 1) + ' '
             continue
         elif append and l.endswith(';'):
             # stop collecting format lines
-            format_lines += l.replace(';','')
+            format_lines += l.replace(';', '')
             append = False
             break
         elif append:
@@ -50,37 +43,40 @@ def parse_format_assignments(txt):
     assignments = thread_last(
         format_lines.split('.'),  # assignment set ends with fmt + dot
         map(lambda x: x.split()),  # break out vars and format
-        (mapcat, lambda y: [(k, y[-1]) for k in y]), # tuple of var, fmt
+        (mapcat, lambda y: [(k, y[-1]) for k in y]),  # tuple of var, fmt
         dict
     )
     return assignments
 
-def block2dict(lines):
-    rqt = re.compile(r'[\"]')  # match quote chars
+
+def block2dict(lines, repl={}):
+    rqt = re.compile(r'[\"\']')  # match quote chars
     rws = re.compile(r'\s')        # match whitespace
     # keep only alnum and a few unreserved symbols
     ruri = re.compile(r'(?![\w\s\-\_\.\'\-\+\(\)\/]|\.).')
     d = thread_last(
         lines,
-        map(lambda x: x.replace('\x92',"'")),
-        map(lambda x: rqt.sub('',x.strip()).split('=')),
-        map(lambda x: (rws.sub('', x[0].strip()), ruri.sub('', x[1].strip()))),
-        filter(lambda x: x[0].find('-') == -1), # no support for ranges
+        map(lambda x: x.replace('\x92', "'")),
+        map(lambda x: rqt.sub('', x.strip()).split('=')),
+        map(lambda x: (rws.sub('', x[0].strip()), ruri.sub('', x[1].strip().lower()))),
+        filter(lambda x: x[0].find('-') == -1),  # no support for ranges
         (mapcat, lambda x: map(lambda y: (y, x[1]), x[0].split(','))),
-        filter(lambda x: x[0].isnumeric()), # remox[1]e non-numeric codes
-        map(lambda x: (int(x[0]), x[1])), # cat codes will be ints
+        filter(lambda x: x[0].isnumeric()),  # remove non-numeric codes
+        map(lambda x: (int(x[0]),  # cat codes are ints
+                       repl[x[1]] if x[1] in repl else x[1])),
         dict
     )
     # d[-1] = np.nan #use NA as a marker for unmapped vals
     return d
 
 
-def parse_variable_labels(txt):
+def parse_variable_labels(txt, repl={}):
+    b2d = curry(block2dict)(repl=repl)
     labels = thread_last(
         txt.split(';'),
         filter(lambda x: x.strip().lower().startswith('value')),
         map(lambda x: x.strip().split('\n')),
-        map(lambda x: (x[0].split()[1].lower(), block2dict(x[1:]))),
+        map(lambda x: (x[0].split()[1].lower(), b2d(x[1:]))),
         dict
     )
     logger.info('parsed varlabels from format txt',
@@ -88,7 +84,7 @@ def parse_variable_labels(txt):
     return labels
 
 
-def load_variable_labels(formas_f, format_f, year=None):
+def load_variable_labels(formas_f, format_f, repl={}, year=None):
     logger.info("loading format labels", file=format_f)
     labels = thread_last(
         format_f,
@@ -96,7 +92,7 @@ def load_variable_labels(formas_f, format_f, year=None):
         lambda x: x.read(),
         lambda t: (t.decode('utf-8', errors='ignore')
                    if type(t) is bytes else t),
-        parse_variable_labels
+        curry(parse_variable_labels)(repl=repl)
     )
     logger.info("loading format assignments", file=formas_f)
     assignments = thread_last(
@@ -113,59 +109,70 @@ def load_variable_labels(formas_f, format_f, year=None):
 def varlabels2df(vlbls, yr=None):
     return thread_last(
         vlbls.items(),
-        map(lambda k,v: pd.DataFrame({'code': list(v.keys()),
-                                      'label': list(v.values()),
-                                      'var': k})),
+        map(lambda k, v: pd.DataFrame({'code': list(v.keys()),
+                                       'label': list(v.values()),
+                                       'var': k})),
         map(lambda df: df.assign(year=yr) if yr else df),
         pd.concat,
-        lambda df: (df.set_index(['var','year','code'])
-                    if yr else df.set_index(['var','code']))
+        lambda df: (df.set_index(['var', 'year', 'code'])
+                    if yr else df.set_index(['var', 'code']))
     )
 
 
 def load_sas_from_zip(fh, format):
-    with zipfile.ZipFile( io.BytesIO(fh.read())) as zipf:
+    with zipfile.ZipFile(io.BytesIO(fh.read())) as zipf:
         with zipf.open(zipf.namelist()[0]) as fh:
             return pd.read_sas(fh, format=format)
 
 
-def load_sas_from_url(url, format):
+def load_sas_from_url(url, format, lgr=logger):
     fh = dl.fetch_data_from_url(url)
-    df = (load_sas_from_zip(fh, format) if url[-3:].lower() == 'zip'
-            else pd.read_sas(fh, format=format))
+    df = (load_sas_from_zip(fh, format)
+          if url[-3:].lower() == 'zip'
+          else pd.read_sas(fh, format=format))
     logger.info("loaded SAS XPORT file", shape=df.shape)
     return df
 
-def load_sas_xport_df(url):
-    df = load_sas_from_url(url, 'xport')
+
+def load_sas_xport_df(url, lgr=logger):
+    df = load_sas_from_url(url, 'xport', lgr)
     df.columns = [x.lower() for x in df.columns]
     return df
 
-def process_sas_survey(meta, facets, prefix, qids, na_syns, client=None):
-    logger.bind(p=prefix)
+
+def process_sas_survey(meta, facets, prefix, qids, na_syns=[],
+                       repl={}, fmts={}, client=None, lgr=logger):
+    lgr.bind(p=prefix)
     md = pd.DataFrame(meta['rows'], columns=meta['cols'])
-    df_munger = curry(sdf.munge_df)(facets=facets,qids=qids,na_syns=na_syns)
-    dfs = map(lambda r: pipe(prefix+r.xpt, delayed(load_sas_xport_df), delayed(df_munger(r=r,
-                                 lbls=load_variable_labels(prefix+r.formas,
-                                                           prefix+r.format)))),
+    df_munger = curry(sdf.munge_df)(facets=facets, qids=qids,
+                                    na_syns=na_syns, fmts=fmts, lgr=lgr)
+    lbl_loader = curry(load_variable_labels)(repl=repl)
+    xpt_loader = curry(load_sas_xport_df)(lgr=lgr)
+    dfs = map(
+        lambda r: pipe(prefix+r.xpt,
+                       delayed(xpt_loader),
+                       delayed(df_munger(r=r,
+                                         lbls=lbl_loader(prefix+r.format,
+                                                         prefix+r.formas)))),
         [r for idx, r in md.iterrows()])
-    logger.info('merging SAS dfs')
+    lgr.info('merging SAS dfs')
     dfs = delayed(pd.concat)(dfs, ignore_index=True)
-    scols = dfs.columns.intersection(set(qids).union(facets))
-    scols = list(scols.compute())
-    logger.info('re-filtering question and facet columns to cast to category dtype', cols=scols)
+    scols = delayed(
+        lambda xf: list(xf.columns
+                          .intersection(set(qids).union(facets))))(dfs)
+    lgr.info('re-filtering question and facet columns to cast to category dtype', cols=scols)
     dfz = (dfs[scols]
            .apply(lambda x: x.astype('category'))
            .reset_index(drop=True)
-           .assign(year = dfs['year'].astype(int),
-                   sitecode = dfs['sitecode'].astype('category'),
-                   weight = dfs['weight'].astype(float),
-                   strata = dfs['strata'].astype(int, errors='ignore'),
-                   psu = dfs['psu'].astype(int, errors='ignore')))
+           .assign(year=dfs['year'].astype(int),
+                   sitecode=dfs['sitecode'].astype('category'),
+                   weight=dfs['weight'].astype(float),
+                   strata=dfs['strata'].astype(int, errors='ignore'),
+                   psu=dfs['psu'].astype(int, errors='ignore'))
+           .reset_index(drop=True))
     dfz.visualize()
-    logger.info('merged SAS dfs')
-    logger.unbind('p')
+    lgr.info('merged SAS dfs')
+    lgr.unbind('p')
     # TODO: expand parallelism to include writing to feather and db
-    return dfz.compute()
-
+    return dfz
 
