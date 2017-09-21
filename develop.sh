6f54d3edc6fa08a8c0b1de70a6d3204d1587fe85
Ajish George <ajish@rootedinsights.com>
2017-09-21 19:16:23 -0400

    clean up reqs, develop script and travis

--- develop.sh
+++ develop.sh
@@ -1,43 +1,64 @@
 #!/bin/bash
 # setup paths for install
+
+export MALLOC_MMAP_THRESHOLD_=1000000
+export MALLOC_MMAP_MAX_=262144
+export MALLOC_MXFAST_=0
+
 export PATH="$HOME/miniconda/bin:$PATH"
+
+CURDIR=`pwd`
+VENV_DIR="venv"
+CONDA_CHAN="intel/label/test"
+CONDA_PYPKGS="pandas scikit-learn cython rpy2"
+CONDA_RPKGS=" r-feather libiconv r-survival r-dbi"
+CONDA_PYVER="python=3.6"
+CONDA_RVER="r-base=3.4.1"
+CONDA_LIST="$CONDA_PYVER $CONDA_RVER $CONDA_PYPKGS $CONDA_RPKGS"
+R_PKGS="c('survey','MonetDB.R')"
+R_REPO="http://r-forge.r-project.org"
+
+COND=`which conda`
 
 setup_miniconda (){
     PLATF=`uname`
     # setup miniconda3
     # wget https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh
-    apt-get update && \
-    apt-get install -y curl gcc wget make g++ openssl libreadline-dev \
-                       libssl-dev libpcre3 libpcre3-dev zlib1g zlib1g-dev \
-                       gfortran
+    sudo apt-get update && \
+    sudo apt-get install -y curl gcc wget make g++ openssl libreadline-dev \
+        libssl-dev libpcre3-dev zlib1g-dev gfortran httpie zsh && \
     wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh \
-        -O miniconda.sh
-    chmod +x miniconda.sh
-    ./miniconda.sh -b -p $HOME/miniconda
+        -O miniconda.sh && \
+    chmod +x miniconda.sh && \
+    yes | ./miniconda.sh -b -p $HOME/miniconda
+}
+
+setup_venv () {
+
+    echo "update conda and add intel channel" && \
+    conda update -y -q conda && \
+    conda info -a && \
+    echo "create conda env with intel python 3.6 and gnu r 3.4.1" && \
+    conda create -y -p $VENV_DIR -c $CONDA_CHAN $CONDA_LIST && \ 
+    echo "install required R packages" && \
+    R --vanilla --slave -e "install.packages($R_PKGS, repos='$R_REPO')" && \
+    echo "activate the env and install package and dev requirements with pip" && \
+    source activate $CURDIR/$VENV_DIR && \
+    pip install -r requirements-dev.txt && \
+    pip install -e .
+
 }
 
 install (){
-    $COND = `which conda`
+
     if [ $COND -eq 0 ]; then
-        echo "found miniconda at $COND, skipping the install..."
+        echo "found conda at $COND, skipping the install..."
     else
         echo "couldn't find miniconda on the path, installing..."
         setup_miniconda
     fi
 
-    CURDIR=`pwd`
-
-    echo "update conda and add intel channel"
-    conda update -q conda
-    conda info -a
-
-    echo "create conda env with intel python 3.6 and gnu r 3.4.1"
-    conda create -p venv -c intel/label/test python=3.6 r-base=3.4.1 pandas scikit-learn cython r-feather libiconv r-survival r-dbi
-
-    echo "activate the env and install package and dev requirements with pip"
-    source activate $CURDIR/venv
-    pip install -r requirements-dev.txt
-    pip install -e .
+    setup_venv
 }
 
 if [ ! -f .develop.lock ]; then
@@ -50,13 +71,6 @@ if [ ! -f .develop.lock ]; then
     fi
 else
     echo "environment exists, getting it ready..."
-    CURDIR=`pwd`
-    export PATH="$HOME/miniconda/bin:$PATH"
-    source activate "$CURDIR/venv"
+    source activate "$CURDIR/$VENV_DIR"
     echo "environment ready!"
 fi
-
-export MALLOC_MMAP_THRESHOLD_=1000000
-export MALLOC_MMAP_MAX_=262144
-export MALLOC_MXFAST_=0
-
